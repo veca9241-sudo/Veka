7b8525b78db06de6ec8d4c92f9800389976ec113
veca9241-sudo <veca9241@gmail.com>
2026-04-14 20:50:36 +0200

    Add files via upload
    Sum of roots of two numbers

---
+++ zbirbrojeva.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+    int prviBroj,drugiBroj,zbir;
+    cout<<"Unesite dva broja"<<endl;
+    cin>>prviBroj;
+    cin>>drugiBroj;
+    zbir=prviBroj+drugiBroj;
+    cout<<"Zbir brojeva"<<prviBroj<<"i"<<drugiBroj<<"iznosi:"<<zbir<<endl;
+    system("pause>0")
+}
