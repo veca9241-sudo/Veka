6718b49c285ae90f987cbc403c746aef7be4c345
veca9241-sudo <veca9241@gmail.com>
2026-04-14 21:01:01 +0200

    Add files via upload

---
+++ veca.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#using namespace std;
+void main()
+
+    int broj;
+cout << "Unesite cijeli broj:<<endl;
+        cin >>
+    broj;
+if (broj % 2 == 0)
+{
+    cout << "Broj je paran." << endl;
+}
+else
+{
+    cout << "Broj je neparan." << endl;
+    cout << "Kvadrat unesenog broja je: " << pow(broj, 2) << endl;
+    elseif()
+    {
+        cout << "Kvadrat korijena unesenog broja je: " << pow(sqrt(broj), 2) << endl;
+    }
+    system("pause>0");
+}
