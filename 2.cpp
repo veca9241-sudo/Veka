1a45733751ba19e918e32a649564c56784d39d9f
veca9241-sudo <veca9241@gmail.com>
2026-04-14 21:11:17 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+void main()
+{
+    float a, b;
+    cin >> a >> b;
+    if (a == true && b == true)
+        cout << "Unesite nove vrijednosti: " << a << " i " << b << endl;
+    int suma = a + b;
+    else(a > 0 || b > 0)
+    {
+        if (a > 0))
+                cout << "Kub broja a je: " << pow(a, 3) << endl;
+        if (b <= 0)
+            cout << "Kub broja b je: " << sqrt(b) << endl;
+
+        system("pause>0");
+    }
+}
