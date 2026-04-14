cfbe32e187b37d61ddda0ccdb8c9807ea2052ddb
veca9241-sudo <veca9241@gmail.com>
2026-04-14 21:10:49 +0200

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+    int a;
+    cin >> a;
+    if (a % 2 == 0)
+        &&(a >= 10 && a = < 99) if (a == true)
+                cout
+            << "Suma je:" << sqrt(a) + pow(a, 2);
+    system("pause>0");
+}
+}
