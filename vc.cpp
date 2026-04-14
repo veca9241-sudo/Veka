efb7f9461a2a2e0c8222b71e74f92e8b9c2f80ad
veca9241-sudo <veca9241@gmail.com>
2026-04-14 21:12:16 +0200

    Add files via upload

---
+++ vc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+    char slovo;
+    int broj;
+    cout << "Unesite jedan karakter: " << endl;
+    cin >> slovo;
+    cout << "Unesite jedan realni broj" << endl;
+    cin >> broj;
+    if (slovo == 'p')
+        cout << "Unijeli ste karakter: " << slovo << endl;
+    cout << "Kvadrat korijena unesenog broja: " << sqrt(pow(broj, 2)) << endl;
+}
+    else(slovo != 'p')
+{
+    cout << "Suma unesenog broja i njegovog kuba: " << suma = broj + pow(broj, 3) << endl;
+
+    system("pause>0");
+}
+}
